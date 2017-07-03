d89a5344e153014a27b6a34c956ce4041aed4931
Seth Baughman <sbaughman1@me.com>
2017-07-03 15:48:59 -0400

    Add Header content and styles

--- src/App.js
+++ src/App.js
@@ -7,22 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <div className="expanded row header">
-          <div className="clearfix">
-            <div className="float-left logo">
-              <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
-            </div>
-            <div className="float-right nav-links">
-              <a href="#">Home</a>
-              <a href="#">About</a>
-              <a href="#">Log in</a>
-            </div>
-          </div>
-          <div className="headline text-center small-12 columns">
-            <h1>The Dwarf Underground</h1>
-            <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
-          </div>
-        </div>
 
         <main className="expanded row">
           <div className="large-8 medium-12 columns article">
