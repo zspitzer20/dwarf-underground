13819943a7c175a5768a43b047c36270d9c7d191
Seth Baughman <sbaughman1@me.com>
2017-07-03 15:43:29 -0400

    Create Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <div className="expanded row header">
           <div className="clearfix">
             <div className="float-left logo">
